4477ecfd54e3778b3384e159f5c1e36cd781e9e2
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-21 20:11:58 +0300

    Adding fucntion that changes the color upon clicking | JS

--- script.js
+++ script.js
@@ -5,33 +5,61 @@ var red_audio = new Audio("./assets/sounds/red.mp3");
 var yellow_audio = new Audio("./assets/sounds/yellow.mp3");
 var wrong_audio = new Audio("./assets/sounds/wrong.mp3");
 
-// Testing
-// var green_test = document.getElementById("green_square") 
-// green_test.addEventListener("click", function(){playSound()});
-
-// function playSound(x){
-//   green_audio.play(); 
-// }
-
-
+// Adding functions that changes the sound (playSound) and color (changeColor) of the elements on click 
 document.getElementById("green_square").addEventListener("click", function(){playSound(1)});
 document.getElementById("red_square").addEventListener("click", function(){playSound(2)});
 document.getElementById("yellow_square").addEventListener("click", function(){playSound(3)});
 document.getElementById("blue_square").addEventListener("click", function(){playSound(4)});
 
+// Function to play the desired sound
 function playSound(x) {
   switch (x) {
     case 1:
       green_audio.play();
+      changeColor(1);
       break;
     case 2:
       red_audio.play();
+      changeColor(2);
       break;
     case 3:
       yellow_audio.play();
+      changeColor(3);
       break;    
     case 4:
       blue_audio.play();
+      changeColor(4);
+      break;
+  }
+}
+// Function that changes the color to white then to the original color after 2ms
+function changeColor(x) {
+  switch (x) {
+    case 1:
+      document.getElementById("green_square").style.backgroundColor = "white";
+      setTimeout(() => {
+        // document.getElementById("green_square").style.display = "none";
+        // document.getElementById("green_square").style.display = "visibility";
+        document.getElementById("green_square").style.backgroundColor = "green";
+      }, 200)
+      break;
+    case 2:
+      document.getElementById("red_square").style.backgroundColor = "white";
+      setTimeout(() => {
+        document.getElementById("red_square").style.backgroundColor = "red";
+      }, 200)
+      break;
+    case 3:
+      document.getElementById("yellow_square").style.backgroundColor = "white";
+      setTimeout(() => {
+        document.getElementById("yellow_square").style.backgroundColor = "yellow";
+      }, 200)
+      break;    
+    case 4:
+      document.getElementById("blue_square").style.backgroundColor = "white";
+      setTimeout(() => {
+        document.getElementById("blue_square").style.backgroundColor = "blue";
+      }, 200)
       break;
   }
 }
