7cfcab42efc136d2a217eb8bf00ae429be63e77f
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-21 18:51:53 +0300

    Adding event listeners to play the sounds on click

--- script.js
+++ script.js
@@ -6,9 +6,32 @@ var yellow_audio = new Audio("./assets/sounds/yellow.mp3");
 var wrong_audio = new Audio("./assets/sounds/wrong.mp3");
 
 // Testing
-var green_test = document.getElementById("green_square") 
-green_test.addEventListener("click", function(){playSound()});
+// var green_test = document.getElementById("green_square") 
+// green_test.addEventListener("click", function(){playSound()});
 
-function playSound(){
-  green_audio.play(); 
+// function playSound(x){
+//   green_audio.play(); 
+// }
+
+
+document.getElementById("green_square").addEventListener("click", function(){playSound(1)});
+document.getElementById("red_square").addEventListener("click", function(){playSound(2)});
+document.getElementById("yellow_square").addEventListener("click", function(){playSound(3)});
+document.getElementById("blue_square").addEventListener("click", function(){playSound(4)});
+
+function playSound(x) {
+  switch (x) {
+    case 1:
+      green_audio.play();
+      break;
+    case 2:
+      red_audio.play();
+      break;
+    case 3:
+      yellow_audio.play();
+      break;    
+    case 4:
+      blue_audio.play();
+      break;
+  }
 }
